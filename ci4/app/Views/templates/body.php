f6e2ac768c656026464badb9edbda198cc1d8dd0
Ichiro Emmanuel Pongos <ipongos@student.apc.edu.ph>
2024-02-16 23:35:29 +0800

    Personal Profile Integration v0.5

--- ci4/app/Views/templates/body.php
+++ ci4/app/Views/templates/body.php
@@ -1,30 +1,14 @@
 <!DOCTYPE html>
 <html>
 <style>
-#center{
-text-align:center;
-}
-
-.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-
-* {
-  box-sizing: border-box;
-}
-body {
-  margin: 0;
+.center:{
+float: center;	
 }
 
 .column {
 float: left;
 width: 50%;
-height: 50%;
 padding: 15px;
-align-content: center;
 }
 
 .row::after {
@@ -38,53 +22,35 @@ clear: both;
     width: 100%;
   }
 }
-
-#intro{
-font-family: Arial, Helvetica, sans-serif;
-font-size: 20px;
-text-align: justify;
-background-color: grey;	
-
-}
-#blend{
-background-color: grey;	
-	
-}
 </style>
 <body>
 <div class="row">
-	<div class="column" id="blend">
-<h1 id="center">About Me</h1>	
-<h2 id="center">Pongos, Ichiro Emmanuel</h3>
+	<div class="column">
+<h2 id="center"> Pongos, Ichiro Emmanuel</h3>
 <?php
 $me = [
     'src'    => 'images/Picture.jpg',
     'alt'    => 'N/a',
     'class'  => 'center',
-    'width'  => '50%',
-    'height' => '50%',
-    'title'  => 'Myself',
+    'width'  => '150',
+    'height' => '150',
+    'title'  => 'That was quite a night',
     'rel'    => 'lightbox',
 ];
 echo img($me);
 ?>
-</div>
-<div class="column" id="intro">	
+	
 <p>I am currently 20 year's old and a year 2 student in <em>Asia Pacific College</em> enrolled on the section <strong>BSIT-MI221</strong>.
 I chose this course because I want to learn more about how to create my own webpages to improve my portfolio and apply them on my future potential jobs. 
 Currently applying a Public School Scholarship to help me when it comes to the school tuition.</p>
-<p>My goals in life right now as a student is to finish school and focus on my career. 
-I want to earn the things I want to accomplish that's possible with my current capabilities and wish to improve them.</p>
-
-</div>
-</div>
+	</div>
+<div class="column">
 <h2>IT Experience: Programming</h2>
 <ul> 
 <li>C</li> 
 <li>Python</li>
 <li>Java</li>
 </ul>
-
 <h2> Hobbies / Interests</h2>
 <ol>
 <li>Drawing Digital Art</li>
@@ -92,6 +58,10 @@ I want to earn the things I want to accomplish that's possible with my current c
 <li>Making recorded videos and editing them</li>
 </ol>
 
-
+<h2>Goals in life</h2>
+<p>My goals in life right now as a student is to finish school and focus on my career. 
+I want to earn the things I want to accomplish that's possible with my current capabilities and wish to improve them.</p>
+</div>
+</div>
 </body>
 </html>
--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,59 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>CodeIgniter Tutorial</title>
-</head>
-<body>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Pongos Site</title>
+<style>
+#Nav{
+list-style-type: none;
+  margin: 0;
+  padding: 0;
+  background-color: #3c3c3c;
+  overflow: hidden;
+  position:fixed;
+  width: 100%;
+}
 
-    <h1><?= esc($title) ?></h1>
+#navbar{
+  float: right;
+}
+
+li a{
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li :hover{
+  background-color: green;
+}
+</style>
+
+
+<ul id="Nav">
+</li>
+  <li id="navbar"><a class="active" href="#intro">About Me</a></li>
+  <li id="navbar"><a href="resource.php">Resources n/a</a></li>
+  <li id="navbar"><a href="Act1.php">Script1Act n/a</a></li>
+  <li id="navbar"><a href="Act2.php">Script2Act n/a</a></li>
+  <li id="navbar"><a href="Act3.php">Script3Act n/a</a></li>
+  <li id="navbar"><a href="form.php">Survey Form n/a</a></li>
+</ul>
+
+<?php
+$me = [
+    'src'    => 'images/BG.jpg',
+    'alt'    => 'N/a',
+    'class'  => 'center',
+    'width'  => '100%',
+    'height' => '850',
+    'title'  => 'WindowsXP',
+    'rel'    => 'lightbox',
+];
+echo img($me);
+?>
+</head>
+</html>
